ede430bb2ee5a5123c0e6b0b6587da423127b0bd
Olive <njerio86@gmail.com>
2023-04-29 09:03:23 +0300

    updated container.js

--- src/Container.js
+++ src/Container.js
@@ -6,8 +6,8 @@ function Container() {
     <div>
       <div className="container">
         <h4 className="about-title">OUR SERVICES</h4>
-        <div className="row mb-5 mt-5">
-          <h4 className="about-title">CAR WARRANTY SERVICES</h4>
+        <h4 className="about-title">CAR WARRANTY SERVICES</h4>
+        <div className="row mb-4 mt-4">
           <div className="col">
             <img
               src="https://myopal.com.my/wp-content/uploads/2021/01/landing-page-motor-insurance.jpg"
@@ -31,36 +31,34 @@ function Container() {
             </button>
           </div>
         </div>
-        <div className="row  mb-5 mt-5">
-          <h4 className="about-title one-a">DATA SCAN SERVICES</h4>
-          <div className="row-a">
-            <div className="col about">
-              <p>
-                Our company provides data scan services for vehicles that aim to
-                identify issues affecting the car's performance using diagnostic
-                tools such as fuel trim, fault code and erased history. The
-                services include a comprehensive analysis of crucial systems to
-                identify problems early on and provide effective solutions. Car
-                owners can save time and money by detecting issues early on. The
-                company assures reliable and accurate diagnostic tools to get
-                the vehicle running smoothly. Interested individuals can contact
-                the company to schedule a data scan service.
-              </p>
-              <button type="button" class="btn btn-info btn-a">
-                Data Scan Services
-              </button>
-            </div>
-            <div className="col">
-              <img
-                src="https://media.istockphoto.com/id/957612802/vector/hardware-diagnostics-condition-of-car-scanning-test-monitoring-analysis-verification.jpg?s=612x612&w=0&k=20&c=GFulPl818cPbtP7H9mdSoYDu7L7TMFyJatM_bwbgWhk="
-                className="img-fluid"
-                alt="vehicle scan"
-              />
-            </div>
+        <h4 className="about-title one-a">DATA SCAN SERVICES</h4>
+        <div className="row row-a mb-4 mt-4">
+          <div className="col about">
+            <p>
+              Our company provides data scan services for vehicles that aim to
+              identify issues affecting the car's performance using diagnostic
+              tools such as fuel trim, fault code and erased history. The
+              services include a comprehensive analysis of crucial systems to
+              identify problems early on and provide effective solutions. Car
+              owners can save time and money by detecting issues early on. The
+              company assures reliable and accurate diagnostic tools to get the
+              vehicle running smoothly. Interested individuals can contact the
+              company to schedule a data scan service.
+            </p>
+            <button type="button" class="btn btn-info btn-a">
+              Data Scan Services
+            </button>
+          </div>
+          <div className="col">
+            <img
+              src="https://media.istockphoto.com/id/957612802/vector/hardware-diagnostics-condition-of-car-scanning-test-monitoring-analysis-verification.jpg?s=612x612&w=0&k=20&c=GFulPl818cPbtP7H9mdSoYDu7L7TMFyJatM_bwbgWhk="
+              className="img-fluid"
+              alt="vehicle scan"
+            />
           </div>
         </div>
-        <div className="row mb-5 mt-5">
-          <h4 className="about-title">GARAGE SERVICES</h4>
+        <h4 className="about-title">GARAGE SERVICES</h4>
+        <div className="row mb-4 mt-4">
           <div className="col">
             <img
               src="https://media.istockphoto.com/id/897283320/vector/car-repair.jpg?s=170667a&w=0&k=20&c=hgBqkHkMe6rlBfLq4n32Jlm7AXC_2e8EV508VKVa0J4="
@@ -85,36 +83,34 @@ function Container() {
             </button>
           </div>
         </div>
-        <div className="row  mb-5 mt-5">
-          <h4 className="about-title one-a">EMERGENCY SERVICES</h4>
-          <div className="row-a">
-            <div className="col about">
-              <p>
-                Our company provides emergency vehicle services that include
-                24/7 roadside assistance and standby core services to help
-                drivers get back on the road quickly. The team of experienced
-                professionals is equipped with the latest tools and techniques
-                to handle any situation. The standby core services provide quick
-                and easy access to replacement parts, reducing wait times for
-                repairs. The services aim to provide peace of mind to drivers in
-                case of unexpected issues. Interested individuals can contact
-                the company to learn more about our emergency vehicle services.
-              </p>
-              <button type="button" class="btn btn-info btn-a">
-                Emergency Services
-              </button>
-            </div>
-            <div className="col">
-              <img
-                src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2022/09/car-being-towed.jpeg.jpg"
-                className="img-fluid"
-                alt="road side assist"
-              />
-            </div>
+        <h4 className="about-title one-a">EMERGENCY SERVICES</h4>
+        <div className="row row-a mb-4 mt-4">
+          <div className="col about">
+            <p>
+              Our company provides emergency vehicle services that include 24/7
+              roadside assistance and standby core services to help drivers get
+              back on the road quickly. The team of experienced professionals is
+              equipped with the latest tools and techniques to handle any
+              situation. The standby core services provide quick and easy access
+              to replacement parts, reducing wait times for repairs. The
+              services aim to provide peace of mind to drivers in case of
+              unexpected issues. Interested individuals can contact the company
+              to learn more about our emergency vehicle services.
+            </p>
+            <button type="button" class="btn btn-info btn-a">
+              Emergency Services
+            </button>
+          </div>
+          <div className="col">
+            <img
+              src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2022/09/car-being-towed.jpeg.jpg"
+              className="img-fluid"
+              alt="road side assist"
+            />
           </div>
         </div>
-        <div className="row mb-5 mt-5">
-          <h4 className="about-title">CAR RENTAL SERVICES</h4>
+        <h4 className="about-title">CAR RENTAL SERVICES</h4>
+        <div className="row mb-4 mt-4">
           <div className="col">
             <img
               src="https://cdn.goseetravel.com/public/Cars/car-keys-hands-rental__ScaleMaxHeightWzQ3MF0.jpg"
